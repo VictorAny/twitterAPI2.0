1211e7c895e7a1f3e0dc87ef2102af89812e1753
Vinh <Vinh>
2016-02-19 18:50:08 -0600

    - Added get request for user endpoint to handle database querying for userID

--- src/com/example/myproject/TwitterAPI2Servlet.java
+++ src/com/example/myproject/TwitterAPI2Servlet.java
@@ -1,6 +1,12 @@
 package com.example.myproject;
 
 import java.io.IOException;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.Key;
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
@@ -12,10 +18,29 @@ public class TwitterAPI2Servlet extends HttpServlet {
 		//The validation team will take the raw request, which is the req parameter, and validate it.
 		//The handlers team will handle fetching that data and then writting the response. 
 		
+		DatastoreService datastore =
+                DatastoreServiceFactory.getDatastoreService();
+		//Should be put in POST requst
+		//Also note that user_id becomes exact key, rather than key_name
+//		Entity user = new Entity("User","user_id");
+//		user.setProperty("name", "Steven");
+//		user.setProperty("user_id", 1);
+//		datastore.put(user);
 		
-		resp.setContentType("text/plain");
-		//This then just prints hello world.
-		//getWriter() function writes to the web page. This will be how you return information
-		resp.getWriter().println("Hello, Twitter 2.0!");
+		String userID = req.getQueryString();
+		if (userID != null){
+			resp.setContentType("text/plain");
+			Key userKey = KeyFactory.createKey("User", userID);
+			Entity userProfile;
+			try {
+				userProfile = datastore.get(userKey);
+				resp.getWriter().println("User found.");
+
+			} catch (EntityNotFoundException e) {
+				resp.getWriter().println("User does not exist");
+			}
+		}else{
+			resp.getWriter().println("Error, userID not valid");
+		}
 	}
 }
