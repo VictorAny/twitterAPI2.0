1f9545662562debe9798fa6e1d1f856332a3cb17
Victor Anyirah <victoranyirah@yahoo.com>
2016-02-21 20:19:48 -0600

    - Finished get request

--- src/com/example/myproject/TwitterAPI2Servlet.java
+++ src/com/example/myproject/TwitterAPI2Servlet.java
@@ -1,6 +1,7 @@
 package com.example.myproject;
 
 import java.io.BufferedReader;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
--- src/com/example/myproject/UserServlet.java
+++ src/com/example/myproject/UserServlet.java
@@ -1,10 +1,12 @@
 package com.example.myproject;
 import com.google.appengine.api.datastore.DatastoreService;
+
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.EntityNotFoundException;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
+import com.google.gson.Gson;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
@@ -20,7 +22,7 @@ public class UserServlet extends TwitterAPI2Servlet{
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		DatastoreService datastore =
+		DatastoreService datastore = 
                 DatastoreServiceFactory.getDatastoreService();
 		String userID = request.getQueryString();
 		if (userID != null){
@@ -29,12 +31,21 @@ public class UserServlet extends TwitterAPI2Servlet{
 			Entity userProfile;
 			try {
 				userProfile = datastore.get(userKey);
-				response.getWriter().println("User found.");
+				Gson json = new Gson();
+				String returnString = json.toJson(userProfile);
+				response.getWriter().println(returnString);
 
 			} catch (EntityNotFoundException e) {
-				response.getWriter().println("User does not exist");
+				this.writeErrorResponse(response, "Error, user does not exist");
 			}
 		}else{
+			//Uncommet if your datastore is empty.
+			//Reminder you can view your datastore at http:localhost:xxxx/_ah/admin where xxxx is your port
+//			Entity testEnt = new Entity("User", "123");
+//			testEnt.setProperty("name", "Victor");
+//			testEnt.setProperty("userName", "Coldsoldier");
+//			testEnt.setProperty("user_id", 12432);
+//			datastore.put(testEnt);
 			this.writeErrorResponse(response, "Error no user ID found");
 		}
 	}
